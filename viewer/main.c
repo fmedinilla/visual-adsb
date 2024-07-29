1004d7a3ae49b0d16b2b8f4df7837802f5efc7cc
Francisco Medinilla Garcia <medinillagarciafrancisco@gmail.com>
2024-07-29 16:32:14 +0200

    viewer: add TTF font

--- viewer/main.c
+++ viewer/main.c
@@ -1,20 +1,27 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
+#include <SDL2/SDL_mouse.h>
+#include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
+#include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_video.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 
 #include "viewer.h"
 #include "plane.h"
 
-#define WIDTH 1920
+#define WIDTH 1080
 #define HEIGTH 1080
 
 #define PLANE_SIZE 32
+#define PLANE_SPEED 0.05
 
 
 typedef struct {
@@ -27,11 +34,24 @@ SDL_Texture *create_texture(const char *filename, SDL_Renderer *ren, SDL_Window 
 void draw_circle(SDL_Renderer *ren, Point center, int radius);
 void draw_moving_line(SDL_Renderer *ren, Point center, int radius);
 void update_planes(Plane *planes);
+int mouse_inside(Point mouse, Plane plane);
+void get_text(SDL_Renderer *ren, int x, int y, char *text, TTF_Font *font, SDL_Texture **texture, SDL_Rect *rect);
 
 int main()
 {
     Viewer *viewer = viewer_create(WIDTH, HEIGTH);
     viewer_set_background(viewer, 20, 20, 20, 255);
+
+    // Fuente
+    TTF_Init();
+    TTF_Font *font = TTF_OpenFont("FreeSans.ttf", 12);
+    SDL_Color white_color = { 255, 255, 255, 255 };
+    //SDL_Surface *text_surf = TTF_RenderText_Solid(font, "Hola Mundo", color);
+    //SDL_Texture *text = SDL_CreateTextureFromSurface(viewer->ren, text_surf);
+
+    //int text_w = text_surf->w;
+    //int text_h = text_surf->h;
+    //SDL_FreeSurface(text_surf);
 
     // Textura avion
     SDL_Texture *plane_tex = create_texture("plane.png", viewer->ren, viewer->win);
@@ -42,14 +62,17 @@ int main()
     const int MAX_PLANES = 3;
     Plane planes[MAX_PLANES];
 
-    planes[0].x = WIDTH - 100;
-    planes[0].y = 0;
+    strcpy(planes[0].id, "A00000");
+    planes[0].x = WIDTH - 800;
+    planes[0].y = 150;
 
-    planes[1].x = 0;
+    strcpy(planes[1].id, "A00010");
+    planes[1].x = 600;
     planes[1].y = 300;
 
+    strcpy(planes[2].id, "A00020");
     planes[2].x = WIDTH-700;
-    planes[2].y = HEIGTH-75;
+    planes[2].y = HEIGTH-200;
 
     SDL_Event event;
 
@@ -57,6 +80,7 @@ int main()
         while(SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) viewer_stop(viewer);
         }
+
 
         viewer_clear(viewer);
 
@@ -72,11 +96,29 @@ int main()
             SDL_RenderCopyEx(viewer->ren, plane_tex, NULL, &plane_coords, planes[i].dir, NULL, SDL_FLIP_NONE);
         }
 
+        // id del avion
+        Point mouse;
+        SDL_GetMouseState(&mouse.x, &mouse.y);
+        // printf("mouse: (%d, %d)\n", mouse.x, mouse.y);
+
+        for (int i = 0; i < MAX_PLANES; i++) {
+            if (mouse_inside(mouse, planes[i])) {
+                // printf("%s\n", planes[i].id);
+
+                SDL_Texture *text;
+                SDL_Rect rect;
+                get_text(viewer->ren, planes[i].x, planes[i].y+PLANE_SIZE, planes[i].id, font, &text, &rect);
+                SDL_RenderCopy(viewer->ren, text, NULL, &rect);
+            }
+        }
+
         // Mostrar lo que se ha dibujado
         viewer_render(viewer);
 
         // update
         update_planes(planes);
+
+        
 
         // 60 FPS
         SDL_Delay(16);
@@ -171,20 +213,48 @@ void update_planes(Plane *planes)
     double theta_radians = 0;
     double theta_degrees = 0;
 
-    planes[0].x += -1;
-    planes[0].y += 1;
-    theta_radians = atan2(1, -1);
+    planes[0].x += -PLANE_SPEED;
+    planes[0].y += PLANE_SPEED;
+    theta_radians = atan2(PLANE_SPEED, -PLANE_SPEED);
     theta_degrees = theta_radians * (180.0 / M_PI);
     planes[0].dir = theta_degrees;
 
-    planes[1].x += 0.5;
-    theta_radians = atan2(0, 0.5);
+    planes[1].x += PLANE_SPEED;
+    theta_radians = atan2(0, PLANE_SPEED);
     theta_degrees = theta_radians * (180.0 / M_PI);
     planes[1].dir = theta_degrees;
 
-    planes[2].x += -0.3;
-    planes[2].y += -1;
-    theta_radians = atan2(-1, -0.3);
+    planes[2].x += -PLANE_SPEED;
+    planes[2].y += -PLANE_SPEED;
+    theta_radians = atan2(-PLANE_SPEED, -PLANE_SPEED);
     theta_degrees = theta_radians * (180.0 / M_PI);
     planes[2].dir = theta_degrees;
+}
+
+int mouse_inside(Point mouse, Plane plane)
+{
+    int x_axis = mouse.x >= plane.x && mouse.x <= (plane.x + PLANE_SIZE);
+    int y_axis = mouse.y >= plane.y && mouse.y <= (plane.y + PLANE_SIZE);
+
+    int inside = x_axis && y_axis;
+    // if (inside) printf("inside\n");
+    return inside;
+}
+
+void get_text(SDL_Renderer *ren, int x, int y, char *text, TTF_Font *font, SDL_Texture **texture, SDL_Rect *rect)
+{
+    int text_w, text_h;
+    SDL_Surface *surface;
+    SDL_Color text_color = {255, 255, 255, 255};
+
+    surface = TTF_RenderText_Solid(font, text, text_color);
+    *texture = SDL_CreateTextureFromSurface(ren, surface);
+    text_w = surface->w;
+    text_h = surface->h;
+    SDL_FreeSurface(surface);
+
+    rect->x = x;
+    rect->y = y;
+    rect->w = text_w;
+    rect->h = text_h;
 }
