2f0f0f68e108892df66be3dfd592b597c56332ef
Francisco Medinilla Garcia <medinillagarciafrancisco@gmail.com>
2024-08-03 20:17:24 +0200

    decoder: Identification messages

--- decoder/main.c
+++ decoder/main.c
@@ -36,50 +36,73 @@ Long message
 #define LONG_MESSAGE_BYTES 14
 #define LONG_MESSAGE_BITS 14 * 8
 
-unsigned int get_block(unsigned char *source, int start, int bits);
+unsigned long get_block(unsigned char *source, int start, int bits);
 void bytes_to_binary_string(unsigned char *bytes, int length, char *output);
 void hexstr_to_bytes(char *hexstr, unsigned char *bytes);
+char identification_code(unsigned char encoded);
 
 
 int main(int argc, char *argv[])
 {
-    char *hexstr = "88ABC12300000000000000000000";
+    char *hexstr = "8D4840D6202CC371C32CE0576098";
+    printf("RAW: %s\n", hexstr);
 
     unsigned char message[LONG_MESSAGE_BYTES] = {0};
     hexstr_to_bytes(hexstr, message);
 
-    char bin_message[LONG_MESSAGE_BITS] = { 0 };
+    char bin_message[LONG_MESSAGE_BITS + 1] = { 0 };
     bytes_to_binary_string(message, LONG_MESSAGE_BYTES, bin_message);
+    printf("BIN: %s\n", bin_message);
 
-    printf("BIN: ");
-    for (int i = 0; i < LONG_MESSAGE_BITS; i++) {
-        printf("%c", bin_message[i]);
-    }
-    printf("\n");
-
-    unsigned int DF = get_block(bin_message, 0, 5);
+    /* BLOQUE DE BITS */
+    unsigned long DF = get_block(bin_message, 0, 5);
     printf("DF: %d\n", DF);
 
-    unsigned int CA = get_block(bin_message, 5, 3);
+    unsigned long CA = get_block(bin_message, 5, 3);
     printf("CA: %d\n", CA);
 
-    unsigned int ICAO = get_block(bin_message, 8, 24);
+    unsigned long ICAO = get_block(bin_message, 8, 24);
     printf("ICAO: 0x%X\n", ICAO);
 
-    unsigned int MESSAGE = get_block(bin_message, 32, 56);
+    unsigned long MESSAGE = get_block(bin_message, 32, 56);
     printf("MESSAGE: 0x%X\n", MESSAGE);
+    
+    unsigned long TC = get_block(bin_message, 32, 5);
+    printf("TC: %d\n", TC);
 
-    unsigned int PARITY = get_block(bin_message, 88, 24);
+    switch (TC)
+    {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            CA = get_block(bin_message, 37, 3);
+            printf("CA: %d\n", CA);
+            
+            int start = 40;
+            int step = 6;
+            for (int i = 0; i < 8; i++) {
+                unsigned long C = get_block(bin_message, start, step);
+                printf("C%d: %d (%c)\n", i+1, C, identification_code(C));
+                start += step;
+            }
+            break;
+    }
+
+   
+
+    unsigned long PARITY = get_block(bin_message, 88, 24);
     printf("PARITY: 0x%X\n", PARITY);
+    /* --------------------------------------------------------------------- */
 
 
     return 0;
 }
 
 
-unsigned int get_block(unsigned char* source, int start, int bits)
+unsigned long get_block(unsigned char* source, int start, int bits)
 {
-    unsigned int block = 0;
+    unsigned long block = 0;
 
     for (int i = 0; i < bits; i++) {
         // Desplazar el bit correspondiente a su posición en el bloque
@@ -105,4 +128,10 @@ void hexstr_to_bytes(char *hexstr, unsigned char *bytes)
     for (int i = 0; i < LEN; i++) {
         sscanf(hexstr + 2 * i, "%2hhx", &bytes[i]);
     }
+}
+
+char identification_code(unsigned char encoded)
+{
+    char decoded[] = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ##### ###############0123456789######";
+    return decoded[encoded];
 }
