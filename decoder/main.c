2bd49b73cbee112ffb67183ed8ea7c7ea632c4bc
Francisco Medinilla Garcia <medinillagarciafrancisco@gmail.com>
2024-08-05 15:38:33 +0200

    decoder: refactor

--- decoder/main.c
+++ decoder/main.c
@@ -2,6 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define _USE_MATH_DEFINES 1
+#include <math.h>
+
+
 #define LONG_MESSAGE_BYTES 14
 #define LONG_MESSAGE_BITS 14 * 8
 
@@ -49,131 +53,114 @@ void bytes_to_binary_string(unsigned char *bytes, int length, char *output);
 void hexstr_to_bytes(char *hexstr, unsigned char *bytes);
 char identification_code(unsigned char encoded);
 void get_data(ADSB_Message *message, char *raw_message);
+void get_aircraft_id(ADSB_Message *message, Aircraft_Identification *aircraft_id);
 
 
 int main(int argc, char *argv[])
 {
-    // Identification: 8D4840D6202CC371C32CE0576098
-    // Airborne position: 8D40621D58C382D690C8AC2863A7 - 8D40621D58C386435CC412692AD6 (10 sec)
+    // Identification: 8D40621D202CC371C32CE0576098
+    // Airborne position: 8D40621D58C382D690C8AC2863A7 - 8D40621D58C386435CC412692AD6 (10 sec) - (lat: 52.2572, lon: 3.91937)
 
-    const int N_MSG = 3;
-    char *messages[] = {
-        "8D40621D202CC371C32CE0576098",
-        "8D40621D58C382D690C8AC2863A7",
-        "8D40621D58C386435CC412692AD6"
-    };
+    ADSB_Message message;
+    get_data(&message, "8D40621D58C382D690C8AC2863A7");
+    printf("ICAO: 0x%02X\n", message.frame.ICAO);
 
-
-    for (int i = 0; i < N_MSG; i++) {
-        ADSB_Message message;
-        get_data(&message, messages[i]);
-
-        printf("Message %d\n", i+1);
-        printf("  ICAO: %02X\n", message.frame.ICAO);
-        printf("  TC: %d\n", message.frame.TC);
-
-        switch (message.frame.TC) {
-            case 0:
-                // None
-                break;
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            {
-                // Aircraft identification
-                unsigned long CA = get_block(message.bin_message, 37, 3);
-                printf("CA: %d\n", CA);
-                
-                int start = 40;
-                int step = 6;
-                for (int i = 0; i < 8; i++) {
-                    unsigned long C = get_block(message.bin_message, start, step);
-                    printf("C%d: %d (%c)\n", i+1, C, identification_code(C));
-                    start += step;
-                }
-                break;
-            }
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-                // Surface position
-                break;
-            case 9:
-            case 10:
-            case 11:
-            case 12:
-            case 13:
-            case 14:
-            case 15:
-            case 16:
-            case 17:
-            case 18:
-            {
-                // Airborne position (w/Baro Altitude)
-                unsigned long SS = get_block(message.bin_message, 38, 2);
-                printf("SS: %d\n", SS);
-
-                unsigned long SAF = get_block(message.bin_message, 40, 1);
-                printf("SAF: %d\n", SAF);
-
-                unsigned long ALT = get_block(message.bin_message, 41, 12);
-                printf("ALT: %d\n", ALT);
-
-                int q_bit = get_block(message.bin_message, 48, 1);
-                printf("Q-Bit: %d\n", q_bit);
-                
-                int N_a = get_block(message.bin_message, 41, 7);
-                int N_b = get_block(message.bin_message, 49, 4);
-                int N = (N_a << 4) | N_b;
-                int h = 25 * N - 1000; // ft
-                printf("Altitude: %d ft\n", h);
-
-                unsigned long T = get_block(message.bin_message, 53, 1);
-                printf("T: %d\n", T);
-
-                unsigned long F = get_block(message.bin_message, 54, 1);
-                printf("F: %d\n", F);
-
-                unsigned long LAT_CPR = get_block(message.bin_message, 55, 17);
-                printf("LAT_CPR: %d\n", LAT_CPR);
-
-                unsigned long LON_CPR = get_block(message.bin_message, 72, 17);
-                printf("LON_CPR: %d\n", LON_CPR);
-
-                break;
-            }
-            case 19:
-                // Airborne velocities
-                break;
-            case 20:
-            case 21:
-            case 22:
-                // Airborne position (w/GNSS Height)
-                break;
-            case 23:
-            case 24:
-            case 25:
-            case 26:
-            case 27:
-                // 	Reserved
-                break;
-            case 28:
-                // 	Aircraft status
-                break;
-            case 29:
-                // 	Target state and status information
-                break;
-            case 30:
-                // None
-                break;
-            case 31:
-                // Aircraft operation status
-                break;
+    switch (message.frame.TC) {
+        case 0:
+            // None
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        {
+            // Aircraft identification - bit 33
+            Aircraft_Identification aircraft_id;
+            get_aircraft_id(&message, &aircraft_id);
+            printf("Aircraft ID: '%s'\n", aircraft_id.id);
+            break;
         }
-    }
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+            // Surface position
+            break;
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+        case 17:
+        case 18:
+        {
+            // Airborne position (w/Baro Altitude) - bit 33
+            //  | 33    | 38    | 40     | 41      | 53   | 54   | 55          | 72          |
+            //  | TC, 5 | SS, 2 | SAF, 1 | ALT, 12 | T, 1 | F, 1 | LAT-CPR, 17 | LON-CPR, 17 |
 
+            unsigned long SS = get_block(message.bin_message, 38, 2);
+            printf("SS: %d\n", SS);
+
+            unsigned long SAF = get_block(message.bin_message, 40, 1);
+            printf("SAF: %d\n", SAF);
+
+            unsigned long ALT = get_block(message.bin_message, 41, 12);
+            printf("ALT: %d\n", ALT);
+
+            int q_bit = get_block(message.bin_message, 48, 1);
+            printf("Q-Bit: %d\n", q_bit);
+            
+            int N_a = get_block(message.bin_message, 41, 7);
+            int N_b = get_block(message.bin_message, 49, 4);
+            int N = (N_a << 4) | N_b;
+            int h = 25 * N - 1000; // ft
+            printf("Altitude: %d ft\n", h);
+
+            unsigned long T = get_block(message.bin_message, 53, 1);
+            printf("T: %d\n", T);
+
+            unsigned long F = get_block(message.bin_message, 54, 1);
+            printf("F: %d\n", F);
+
+            unsigned long LAT_CPR = get_block(message.bin_message, 55, 17);
+            printf("LAT_CPR: %d\n", LAT_CPR);
+
+            unsigned long LON_CPR = get_block(message.bin_message, 72, 17);
+            printf("LON_CPR: %d\n", LON_CPR);
+
+            break;
+        }
+        case 19:
+            // Airborne velocities
+            break;
+        case 20:
+        case 21:
+        case 22:
+            // Airborne position (w/GNSS Height)
+            break;
+        case 23:
+        case 24:
+        case 25:
+        case 26:
+        case 27:
+            // 	Reserved
+            break;
+        case 28:
+            // 	Aircraft status
+            break;
+        case 29:
+            // 	Target state and status information
+            break;
+        case 30:
+            // None
+            break;
+        case 31:
+            // Aircraft operation status
+            break;
+    }
     
     return 0;
 }
@@ -230,4 +217,26 @@ void get_data(ADSB_Message *message, char *raw_message)
     message->frame.ME = get_block(message->bin_message, 33, 56);
     message->frame.PI = get_block(message->bin_message, 89, 24);
     message->frame.TC = get_block(message->bin_message, 33, 5);
+}
+
+void get_aircraft_id(ADSB_Message *message, Aircraft_Identification *aircraft_id)
+{
+    // Aircraft identification - bit 33
+    // | 33   | 38   | 41   | 47   | 53   | 59   | 65   | 71   | 77   | 83   | 
+    // | TC,5 | CA,3 | C1,6 | C2,6 | C3,6 | C4,6 | C5,6 | C6,6 | C7,6 | C8,6 |
+
+    unsigned long CA = get_block(message->bin_message, 38, 3);
+    char id[9] = {0};
+
+    int start = 41;
+    int step = 6;
+    for (int i = 0; i < 8; i++) {
+        unsigned long C = get_block(message->bin_message, start, step);
+        id[i] = identification_code(C);
+        start += step;
+    }
+    id[8] = '\0';
+
+    aircraft_id->CA = CA;
+    strcpy(aircraft_id->id, id);
 }
