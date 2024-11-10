12e48966832b0ea6ebf7870bd9db866db4420591
Francisco Medinilla Garcia <medinillagarciafrancisco@gmail.com>
2024-11-10 09:47:17 +0100

    add generator skeleton

---
+++ generator/generator.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "generator.h"
+
+void _generator_test()
+{
+    puts("files\n  - generator.h\n  - generator.c");
+}
---
+++ generator/main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include "generator.h"
+
+
+int main(void)
+{
+    _generator_test();
+    return EXIT_SUCCESS;
+}
